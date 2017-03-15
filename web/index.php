f371c18a489b8f5577130b338922618ecc4f4d3d
Łukasz Segin <lukasz-segin@o2.pl>
2017-03-15 22:48:29 +0100

    Create basic flow of application.

---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+  $controller = isset($_GET['controller']) ? $_GET['controller'] : 'home';
+
+
+  $dir = __DIR__ . '/../src/app/controllers/';
+
+  if(file_exists($dir . $controller . '.php')) {
+    require $dir . $controller . '.php';
+  } else {
+    header('HTTP/1.0 404 Not Found');
+    echo '404';
+  }
